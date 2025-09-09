b7554be3d47e056029efff2be7f73651ace1bc68
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-09-09 12:58:22 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ connectToDb();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://sakibchatbox1.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
