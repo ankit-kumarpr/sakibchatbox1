6216f4d38055b37510432c98594cd59af38fad89
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-09-09 12:58:37 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 // Setup Socket.IO with proper CORS
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // frontend URL
+    origin: "https://sakibchatbox1.vercel.app", // frontend URL
     methods: ["GET", "POST"],
     credentials: true,
   },
